bb0a5ff40573950ccfe8eb469a7e06e8a9f248aa
Ruslan Littlewell Home <mrlittlewell@gmail.com>
2019-11-22 02:14:00 +0300

    update resourse

--- js/init.js
+++ js/init.js
@@ -3,6 +3,11 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.carousel').carousel({
+      dist: -50,
+      numVisible: 3,
+      shift: 100
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
