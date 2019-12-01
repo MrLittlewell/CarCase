f878322befa008ab630540ba5b9178d4da5a3787
Ruslan Littlewell Home <mrlittlewell@gmail.com>
2019-12-01 13:37:54 +0300

    update v1

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.modal').modal();
+    $('.tooltipped').tooltip();
     $('.slider').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -13,3 +14,109 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$('.case-colors div').click(function() {
+  $('.case-colors div.active').removeClass('active');
+  $(this).addClass('active');
+})
+/******EcoKozha Line****/
+$('#modal1 .bej').click(function() {
+  $('#modal1 .prewiew img').attr("src","/images/products/ecolather/avtolider/bejeviy-650x650.jpg")
+})
+$('#modal1 .bel').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/beliy-650x650.jpg");
+})
+$('#modal1 .cher').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherniy-650x650.jpg");
+})
+$('#modal1 .cherbej').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-bejeviy-650x650.jpg");
+})
+$('#modal1 .chercrem').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-kremoviy-650x650.jpg");
+})
+$('#modal1 .cherbel').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-beliy-650x650.jpg");
+})
+$('#modal1 .cherkorich').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-fox-650x650.jpg");
+})
+$('#modal1 .cherkrasn').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-krasniy-650x650.jpg");
+})
+$('#modal1 .cherojeltiy').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-jeltiy-650x650.jpg");
+})
+$('#modal1 .cheroranj').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-orange-650x650.jpg");
+})
+$('#modal1 .cherschok').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-shokolad-650x650.jpg");
+})
+$('#modal1 .cherser').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-seriy-650x650.jpg");
+})
+$('#modal1 .chersin').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-siniy-650x650.jpg");
+})
+$('#modal1 .chertser').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/cherno-tseriy-650x650.jpg");
+})
+$('#modal1 .korich').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/fox-650x650.jpg");
+})
+$('#modal1 .krasniy').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/krasniy-650x650.jpg");
+})
+$('#modal1 .krem').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/kremoviy-650x650.jpg");
+})
+$('#modal1 .schok').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/shokoladniy-650x650.jpg");
+})
+$('#modal1 .ser').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/seriy-650x650.jpg");
+})
+$('#modal1 .serbej').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/seriy-bejeviy-650x650.jpg");
+})
+$('#modal1 .serkrem').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/seriy-kremoviy-650x650.jpg");
+})
+$('#modal1 .shokkrem').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/shokoladniy-kremoviy-650x650.jpg");
+})
+$('#modal1 .tkorich').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/paprika-650x650.jpg");
+})
+$('#modal1 .tser').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/temnoseriy-650x650.jpg");
+})
+$('#modal1 .tserser').click(function() {
+  $('#modal1 .prewiew img').attr("src", "/images/products/ecolather/avtolider/temnoseriy-svetloseriy-650x650.jpg");
+})
+
+/********************************** */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
